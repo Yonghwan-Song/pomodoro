8f6970022ad6adf371ff31d5a84f756805a9a6b2
Yonghwan Song <shwanpg@gmail.com>
2022-08-06 20:33:05 -0400

    Fix import issue in Timer.jsx and reducers.js

--- client/src/Components/reducers.js
+++ client/src/Components/reducers.js
@@ -1,4 +1,53 @@
-export function reducer(state, action) {
+export function reducerTimer(state, action) {
+  switch (action.type) {
+    case "start":
+      return {
+        ...state,
+        running: true,
+        startTime: action.payload,
+      };
+    case "pause":
+      return {
+        ...state,
+        running: false,
+        pause: {
+          ...state.pause,
+          record: [...state.pause.record, { start: action.payload }],
+        },
+      };
+    case "resume":
+      return {
+        ...state,
+        running: true,
+        pause: {
+          record: state.pause.record.map((obj) => {
+            if (obj.end === undefined) {
+              return {
+                ...obj,
+                end: action.payload,
+              };
+            } else {
+              return obj;
+            }
+          }),
+          totalLength:
+            state.pause.totalLength +
+            (action.payload -
+              state.pause.record[state.pause.record.length - 1].start),
+        },
+      };
+    case "reset":
+      return {
+        running: false,
+        startTime: 0,
+        pause: { totalLength: 0, record: [] },
+      };
+    default:
+      throw new Error();
+  }
+}
+
+export function reducerPatternTimer(state, action) {
   switch (action.type) {
     case "start":
       return {
@@ -45,4 +94,5 @@ export const ACTION = {
   START: "start",
   PAUSE: "pause",
   RESUME: "resume",
+  RESET: "reset",
 };
