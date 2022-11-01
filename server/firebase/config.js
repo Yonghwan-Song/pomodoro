942c8818cd688fe4ad065c3055fa4359eca9242d
Yonghwan Song <shwanpg@gmail.com>
2022-10-31 21:39:23 -0400

    Change the strategy of importing json in es6
    assert {type: "json"} statement caused error while deploying the api
    server.

--- server/firebase/config.js
+++ server/firebase/config.js
@@ -1,11 +1,24 @@
+import { readFile } from "fs/promises";
 import admin from "firebase-admin";
 
 //(node:4166) ExperimentalWarning: Importing JSON modules is an experimental feature. This feature could change at any time
-import serviceAccount from "./pomodoro-ef5e0-firebase-adminsdk-3ulzg-01c915ff0d.json" assert { type: "json" };
+//import serviceAccount from "./pomodoro-ef5e0-firebase-adminsdk-3ulzg-01c915ff0d.json" assert { type: "json" };
+let serviceAccount;
+try {
+  const filePath = new URL(
+    "./pomodoro-ef5e0-firebase-adminsdk-3ulzg-01c915ff0d.json",
+    import.meta.url
+  );
+  serviceAccount = await readFile(filePath, { encoding: "utf8" });
+} catch (error) {
+  console.log(`-----------------------config.js-----------------------`);
+  console.log(error.message);
+}
 
 //console.log(serviceAccount.type);
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  // credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(serviceAccount)),
 });
 
 export default admin;
