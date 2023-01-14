cb2a775edda0413a321734bd55ab353342285c3e
Yonghwan Song <shwanpg@gmail.com>
2023-01-13 21:09:51 -0500

    Convert Button.jsx to Button.tsx

---
+++ client/src/Components/Buttons/Button.tsx
@@ -0,0 +1,14 @@
+type ButtonProps = {
+  handleClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  children: React.ReactNode;
+  color: string;
+  type: "button" | "submit" | "reset" | undefined;
+};
+
+export function Button({ children, type, color, handleClick }: ButtonProps) {
+  return (
+    <button type={type} className={`btn btn-${color}`} onClick={handleClick}>
+      {children}
+    </button>
+  );
+}
