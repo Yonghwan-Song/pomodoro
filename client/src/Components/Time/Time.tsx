68a1bc21a3a78854aa4a8f2a331b97e10998f2d1
Yonghwan Song <shwanpg@gmail.com>
2023-09-06 14:56:47 +0900

    Create Minute and Second components to be composed in the Time component

--- client/src/Components/Time/Time.tsx
+++ client/src/Components/Time/Time.tsx
@@ -1,30 +1,17 @@
-import styles from "./Time.module.css";
+import Second from "./Second/Second";
+import Mintue from "./Minute/Mintue";
 
 type TimeProps = {
   seconds: number;
 };
 
 export default function Time({ seconds }: TimeProps) {
-  let a = 0,
-    b = 0,
-    c = 0,
-    d = 0;
-
   let min = Math.floor(seconds / 60);
   let sec = seconds % 60;
 
-  a = Math.floor(min / 10);
-  b = min % 10;
-  c = Math.floor(sec / 10);
-  d = sec % 10;
-
   return (
     <>
-      <div className={styles.div}>{a}</div>
-      <div className={styles.div}>{b}</div>
-      <div className={styles.div}>:</div>
-      <div className={styles.div}>{c}</div>
-      <div className={styles.div}>{d}</div>
+      <Mintue minutes={min} />:<Second seconds={sec} />
     </>
   );
 }
