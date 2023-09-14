f792e95d853117e6303222aa9b93a3de00abddef
Yonghwan Song <shwanpg@gmail.com>
2023-09-14 21:45:37 +0900

    Revise conditional rendering in Main.tsx.
    Now, loading message appears only when the pomoSetting object is not ready.

--- client/src/Pages/Main/Main.tsx
+++ client/src/Pages/Main/Main.tsx
@@ -58,27 +58,36 @@ export default function Main() {
     }
   }, [user, pomoSetting]);
 
-  const isDataReady =
-    !!Object.entries(pomoSetting).length && statesRelatedToTimer !== null;
+  // When this main page is loaded,
+  // pomoSetting is fetched from a remote server unlike the statesRelatedToTimer is retrieved from a browser's storage (client side).
+  // Therefore, I just want to show "loading timer..." message only when the pomoSetting is not ready.
+  // Reason:
+  // User experience에 유의미하게 영향을 주는 요소는 여기서
+  // pomoSetting object를 가져오는 데 걸리는 시간이다.
+  // 왜냐하면 이것은 애초에 remote server에서 가져오는 데이터이기 때문이다(비록 cache를 하더라도).
+  // statesRelatedToTimer는 user가 사용하는 브라우저에 저장되기 때문에 준비하는 데 걸리는 시간은 유의미한 영향을 주지 않는다.
+  const isPomoSettingReady = !!Object.entries(pomoSetting).length;
+  const isStatesRelatedToTimerReady = statesRelatedToTimer !== null;
 
   return (
     <main>
       <RecOfToday records={records} />
       <section>
-        {isDataReady ? (
-          <PatternTimer
-            statesRelatedToTimer={statesRelatedToTimer}
-            pomoDuration={pomoSetting.pomoDuration}
-            shortBreakDuration={pomoSetting.shortBreakDuration}
-            longBreakDuration={pomoSetting.longBreakDuration}
-            numOfPomo={pomoSetting.numOfPomo}
-            setRecords={setRecords}
-          />
-        ) : (
-          <StyledLoadingMessage top="51%">
-            loading timer...
-          </StyledLoadingMessage>
-        )}
+        {isStatesRelatedToTimerReady &&
+          (isPomoSettingReady ? (
+            <PatternTimer
+              statesRelatedToTimer={statesRelatedToTimer}
+              pomoDuration={pomoSetting.pomoDuration}
+              shortBreakDuration={pomoSetting.shortBreakDuration}
+              longBreakDuration={pomoSetting.longBreakDuration}
+              numOfPomo={pomoSetting.numOfPomo}
+              setRecords={setRecords}
+            />
+          ) : (
+            <StyledLoadingMessage top="51%">
+              loading timer...
+            </StyledLoadingMessage>
+          ))}
       </section>
     </main>
   );
