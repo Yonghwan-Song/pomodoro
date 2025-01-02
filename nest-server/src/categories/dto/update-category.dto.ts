08ab20f3d4f5f2b587685d6f8765b45a48c98b36
Yonghwan Song <shwanpg@gmail.com>
2025-01-02 15:49:14 +0900

    Update UpdateCategoryDto class

--- nest-server/src/categories/dto/update-category.dto.ts
+++ nest-server/src/categories/dto/update-category.dto.ts
@@ -1,33 +1,74 @@
-import { IsString } from 'class-validator';
+import {
+  IsString,
+  ValidateNested,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  Validate,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+//#region Original
+// export class UpdateCategoryDto {
+//   @IsString()
+//   // @IsNotEmpty()
+//   name: string;
+
+//   // option 1
+//   // data: Data;
+
+//   // option 2
+//   // data:
+//   //   | {
+//   //       name: string;
+//   //     }
+//   //   | { color: string }
+//   //   | { isCurrent: boolean }
+//   //   | { isOnStat: boolean };
+//   data: {
+//     name?: string;
+//     color?: string;
+//     isCurrent?: boolean;
+//     isOnStat?: boolean;
+//   };
+// }
+//#endregion
+
+class CategoryData {
+  @IsString()
+  name?: string;
+
+  @IsString()
+  color?: string;
+
+  isCurrent?: boolean;
+
+  isOnStat?: boolean;
+}
+
+@ValidatorConstraint({ name: 'atLeastOneProperty', async: false })
+class AtLeastOnePropertyValidator implements ValidatorConstraintInterface {
+  validate(data: CategoryData) {
+    if (!data || typeof data !== 'object') {
+      return false;
+    }
+
+    return (
+      Object.keys(data).length > 0 &&
+      Object.values(data).some((value) => value !== undefined)
+    );
+  }
+
+  defaultMessage() {
+    return 'data object must contain at least one property';
+  }
+}
 
 export class UpdateCategoryDto {
   @IsString()
-  // @IsNotEmpty()
   name: string;
 
-  // option 1
-  // data: Data;
-
-  // option 2
-  data:
-    | {
-        name: string;
-      }
-    | { color: string }
-    | { isCurrent: boolean }
-    | { isOnStat: boolean };
+  @ValidateNested()
+  @Validate(AtLeastOnePropertyValidator)
+  @Type(() => CategoryData)
+  data: CategoryData;
 }
-
-// class Data {
-//   @IsString()
-//   @IsOptional()
-//   name?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   color?: string;
-
-//   @IsString()
-//   @IsOptional()
-//   isCurrent?: boolean;
-// }
